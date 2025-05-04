87b0b4b016114b8ac682725f4a740d680966b2f0
luisa <lfc20082019@gmail.com>
2025-05-04 14:53:15 -0500

    cambio base de datos

--- peritajes/settings.py
+++ peritajes/settings.py
@@ -108,8 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['Peritajes_app/Public']
-MEDIA_ROOT =['Peritajes_app/Public']
 
 
 
