166a08b4499c02453fe9510847939e0e09e2002d
luisa <lfc20082019@gmail.com>
2025-05-11 11:20:00 -0500

    reparacion

--- peritajes/settings.py
+++ peritajes/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'peritajes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'peritajes_app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
